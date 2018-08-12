827612569f3c53018c95bd00c967763ad86d0d96
Mikhail <m.chuenkov@gmail.com>
2018-08-12 16:37:33 +0300

    Добавил функционал корзины в 'shopping cart.html'

--- cart-p.js
+++ cart-p.js
@@ -64,50 +64,204 @@ function renderGoodsList() {
 function renderCart() {
   $("#cart").empty();
   $("#cart-total__cart-img").empty();
+  $("#table-cart").empty();
+  $('.total-content-pink, #subTotal').empty();
   $.get("http://localhost:3000/cart-p/", {}, function (goods) {
     let $cart = $('#cart');
+    let $tablecart = $("#table-cart");
     let total = 0;
+    $tablecart.append(
+      $('<tr />',{
+        class: 'table-cart-title',
+      })
+        .append(
+          $('<th />',{
+            class: 'table-cart-first',
+            text: 'PROUCT DETAILS'
+          })
+        )
+        .append(
+          $('<th />',{
+            class: 'table-cart-second',
+            text: 'UNITE PRICE'
+          })
+        )
+        .append(
+          $('<th />',{
+            class: 'table-cart-second',
+            text: 'QUANTITY'
+          })
+        )
+        .append(
+          $('<th />',{
+            class: 'table-cart-second',
+            text: 'SHIPPING'
+          })
+        )
+        .append(
+          $('<th />',{
+            class: 'table-cart-second',
+            text: 'SUBTOBAL'
+          })
+        )
+        .append(
+          $('<th />',{
+            class: 'table-cart-second',
+            text: 'ACTION'
+          })
+        )
+    );
     goods.forEach(function (item) {
-      $cart
+      $cart.append(
+          $("<div />", {
+            class: 'goods-box'
+          })
+            .append(
+              $("<a />", {
+                href: "single%20page.html"
+              })
+                .append(
+                  $("<img />", {
+                    src: item.img,
+                    class: 'goods__cart-img'
+                  })
+                )
+            )
+            .append(
+              $("<div />", {
+                class: 'goods-box-param goods-box-param__cart-img'
+              })
+                .append(
+                  $("<a />", {
+                    href: "single%20page.html",
+                    class: "goods-title goods-title__cart-img",
+                    text: item.name,
+                  })
+                )
+                .append(
+                  $("<img />", {
+                    class: 'goods-stars goods-stars__cart-img',
+                    src: "img/stars.png",
+                  })
+                )
+                .append(
+                  $("<span />", {
+                    class: "goods-price goods-price__cart-img",
+                    text: item.quantity + ' x $' + +item.quantity * +item.price,
+                  })
+                )
+            )
+            .append(
+              $("<div />", {
+                class: 'table-cart-action table-cart-action__cart-img'
+              })
+                .append(
+                  $("<a />", {
+                    href: "#",
+                    class: 'remove',
+                    "data-id": item.id,
+                    "data-price": item.price,
+                    "data-name": item.name,
+                    "data-quantity": item.quantity
+                  })
+                    .append(
+                      $("<div />", {
+                        class: 'table-cart-action-circle'
+                      })
+                        .append(
+                          $("<div />", {
+                            class: 'cross1 cross__center'
+                          })
+                        )
+                        .append(
+                          $("<div />", {
+                            class: 'cross2 cross__center'
+                          })
+                        )
+                    )
+                )
+            )
+        );
+      $tablecart.append(
+        $('<tr />', {
+          class: 'table-cart-good',
+        })
           .append(
-            $("<div />", {
-              class: 'goods-box'
+            $('<td />', {
+              class: 'table-cart-good-box',
             })
               .append(
-                $("<a />", {
+                $('<a />', {
                   href: "single%20page.html"
                 })
                   .append(
-                    $("<img />", {
+                    $('<img />', {
                       src: item.img,
-                      class: 'goods__cart-img'
+                      class: 'goods__cart-img',
                     })
                   )
+                  .append(
+                    $('<div />')
+                      .append(
+                        $('<h2 />', {
+                          text: item.name,
+                        })
+                      )
+                      .append(
+                        $('<div />', {
+                          class: 'table-cart-good-box-param',
+                        })
+                          .append(
+                            $('<h3 />', {
+                              text: 'Color',
+                            })
+                          )
+                          .append(
+                            $('<h5 />', {
+                              text: 'Red',
+                            })
+                          )
+                          .append(
+                            $('<h3 />', {
+                              text: 'Size',
+                            })
+                          )
+                          .append(
+                            $('<h5 />', {
+                              text: 'XL',
+                            })
+                          )
+                      )
+                  )
               )
+          )
+          .append(
+            $('<td />', {
+              text: '$' + item.price,
+            })
+          )
+          .append(
+            $('<td />')
               .append(
-                $("<div />", {
-                  class: 'goods-box-param goods-box-param__cart-img'
-                })
-                  .append(
-                    $("<a />", {
-                      href: "single%20page.html",
-                      class: "goods-title goods-title__cart-img",
-                      text: item.name,
-                    })
-                  )
-                  .append(
-                    $("<img />", {
-                      class: 'goods-stars goods-stars__cart-img',
-                      src: "img/stars.png",
-                    })
-                  )
-                  .append(
-                    $("<span />", {
-                      class: "goods-price goods-price__cart-img",
-                      text: item.quantity + ' x $' + +item.quantity * +item.price,
-                    })
-                  )
+                $('<input>', {
+                type: 'text',
+                placeholder: item.quantity,
+                  pattern: "[0-9]",
+              })
               )
+          )
+          .append(
+            $('<td />', {
+              text: 'FREE',
+            })
+          )
+          .append(
+            $('<td />', {
+              text: '$' + +item.quantity * +item.price,
+            })
+          )
+          .append(
+            $('<td />')
               .append(
                 $("<div />", {
                   class: 'table-cart-action table-cart-action__cart-img'
@@ -138,15 +292,10 @@ function renderCart() {
                       )
                   )
               )
-          );
-
+          )
+      );
       total += +item.quantity * +item.price;
     });
-    // $cartMega.append(
-    //   $("<div />", {
-    //     class: 'table-cart-action-circle'
-    //   })
-    // );
     $("#cart").append($cart);
     $('#cart-total__cart-img').append(
       $("<div />", {
@@ -160,10 +309,12 @@ function renderCart() {
           text: '$' + total
         })
       );
+    $('.total-content-pink, #subTotal').append(
+      $('<span />', {
+        text: '$' + total,
+      })
+    )
 
-    //$("#mega__cart-img").append($cart);
-    //$("#cart").append($cart);
-    //$("#cart").append($("<div />", {text: "Total $" + total}))
   }, "json");
 }
 
@@ -174,7 +325,7 @@ function renderCart() {
     /**
      * Удаляем товар из корзины
      */
-    $("#cart").on("click", ".remove", function (event) {
+    $("#cart, #table-cart").on("click", ".remove", function (event) {
       $.ajax({
         type: "DELETE",
         url: "http://localhost:3000/cart-p/" + $(this).attr("data-id"),
